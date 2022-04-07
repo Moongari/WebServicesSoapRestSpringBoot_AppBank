a304d32b8f6dacab12d3325c2ebdabcfa853b2aa
Moustafa moungari <moungari.moustafa@gmail.com>
2022-04-07 16:19:22 +0200

    initial commit projet banque

---
+++ src/main/java/com/moon/ms_banque/MsBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.moon.ms_banque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBanqueApplication.class, args);
+    }
+
+}
