82f8bd144bc47acf7987154cbedaaa1ca9f403d6
Moustafa moungari <moungari.moustafa@gmail.com>
2022-04-08 19:07:26 +0200

    ajout d'une recherche par type de compte

--- src/main/java/com/moon/ms_banque/repository/CompteRepository.java
+++ src/main/java/com/moon/ms_banque/repository/CompteRepository.java
@@ -1,12 +1,18 @@
 package com.moon.ms_banque.repository;
 
 import com.moon.ms_banque.entities.Compte;
+import com.moon.ms_banque.entities.TypeCompte;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 //=== utilisation de Spring Data Rest =====
 @RepositoryRestResource
 public interface CompteRepository extends JpaRepository<Compte,Long>
 {
+    //== on peut rechercher cela de cette facon
+    //== http://localhost:8082/api/comptes/search/findByType?type=COURANT grace spring Data Rest==
+    List<Compte> findByType(TypeCompte type);
 
 }
