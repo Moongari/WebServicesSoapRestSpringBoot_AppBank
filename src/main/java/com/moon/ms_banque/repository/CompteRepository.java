37b6f28a806d668a170b367fc14ef4a041b6b499
Moustafa moungari <moungari.moustafa@gmail.com>
2022-04-11 08:32:22 +0200

    ajout de nouvelles annotations

--- src/main/java/com/moon/ms_banque/repository/CompteRepository.java
+++ src/main/java/com/moon/ms_banque/repository/CompteRepository.java
@@ -3,7 +3,9 @@ package com.moon.ms_banque.repository;
 import com.moon.ms_banque.entities.Compte;
 import com.moon.ms_banque.entities.TypeCompte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
@@ -13,6 +15,9 @@ public interface CompteRepository extends JpaRepository<Compte,Long>
 {
     //== on peut rechercher cela de cette facon
     //== http://localhost:8082/api/comptes/search/findByType?type=COURANT grace spring Data Rest==
-    List<Compte> findByType(TypeCompte type);
+    // nous pouvons egalement utilisé ceci pour definir nos propres parametre url
+    // http://localhost:8082/api/comptes/search/byType?t=EPARGNE
+    @RestResource(path = "/byType")
+    List<Compte> findByType(@Param("t") TypeCompte type);
 
 }
